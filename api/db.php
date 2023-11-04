7a77d770e25d40f1a2fb6c0231d6058ee411bcd1
Cade Wilson <cade25wilson@gmail.com>
2023-11-04 12:02:49 -0600

    db connection + env gitignore

---
+++ api/db.php
@@ -0,0 +1,8 @@
+<?php
+try {
+    $db = new PDO('sqlite:' . getenv('DATABASE'));
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+    die();
+}
